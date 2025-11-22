b385be7d2a189b9b0ba266741be18781718501b1
Jagbir Singh <jg581261@dal.ca>
2025-11-21 22:33:29 -0400

    day1

---
+++ Day1/revserse.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class revserse {
+    public static void main(String[] args) {
+        int[] arr = {3,5,1,2,7,6,4};
+        int temp = 0;
+        for(int left=0, right = arr.length-1; left<right; left++, right--){
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
